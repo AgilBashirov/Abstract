36b6a510a24f0be0c94b959d4b7d927e2f4cbea7
Agil Bashirov <agilab@code.edu.az>
2022-12-30 23:06:42 +0400

    Add project files.

---
+++ Abstract/Animal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    abstract class Animal
+    {
+        public Animal()
+        {
+            Console.WriteLine("Abstract constructor ishe dushdu.");
+        }
+        public abstract void Eat();
+    }
+    class Dog : Animal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("Eat as Dog");
+        }
+    }
+    class Horse : Animal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("Eat as Horse");
+
+        }
+    }
+
+
+}
---
+++ Abstract/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Dog dog = new Dog();
+            dog.Eat();
+
+            Horse horse = new Horse();
+            horse.Eat();
+        }
+    }
+}
